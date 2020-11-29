10248f8dcf1f7a641c2be4bc959bb3aa10626040
david.mccabe <david.mccabe082@gmail.com>
2020-11-29 20:57:24 +0000

    fix ship label alignment

--- src/components/ship.js
+++ src/components/ship.js
@@ -5,8 +5,8 @@ export function Ship(props) {
     const symbol = props.symbol;
 
     return (
-        <div className="ship">
-            <p>{symbol}</p>
+        <div>
+            <p className="ship">{symbol}</p>
         </div>
     );
 }
